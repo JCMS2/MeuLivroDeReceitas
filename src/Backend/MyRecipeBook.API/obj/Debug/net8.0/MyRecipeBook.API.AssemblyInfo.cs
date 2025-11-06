71c819cd772482f04a615ba2f79f7b61104f2627
Jose Carlos <jc270922@gmail.com>
2025-11-06 17:27:49 -0300

    Post

--- src/Backend/MyRecipeBook.API/obj/Debug/net8.0/MyRecipeBook.API.AssemblyInfo.cs
+++ src/Backend/MyRecipeBook.API/obj/Debug/net8.0/MyRecipeBook.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRecipeBook.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300dfaa8c8313d20dfb477c61f0dc071dd44a094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2c291f3aca9abffc7c6dbdd6703149549420b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRecipeBook.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRecipeBook.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
